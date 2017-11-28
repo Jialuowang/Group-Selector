1c1f2e959c0d00f7e94495d2204b9830ef6f4fde
Ethan Walters <ethan.walters@mullenmail.com>
2017-11-28 14:33:50 -0700

    create splitting groups class , group selector class is runner

--- Group Selector/src/groupSelector.java
+++ Group Selector/src/groupSelector.java
@@ -1,5 +1,19 @@
+import java.util.*;
 
 public class groupSelector
 	{
+	public static int numberOfGroups = 0;
+	
+	public static void main(String[] args) 
+		{
+		askUserHowManyGroups();
+		}
 
+	private static void askUserHowManyGroups() 
+		{
+		Scanner numOfGroups = new Scanner(System.in);
+		System.out.println("How many groups do you want?");
+		numberOfGroups = numOfGroups.nextInt();
+		System.out.println("Awesome");
+		}
 	}
